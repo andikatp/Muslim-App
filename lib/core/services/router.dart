0151250af554aa6136477e789517eb8c49b9cdb1
andikatp <triprasetya_andika@yahoo.com>
2024-01-30 17:01:28 +0700

    di for quran

--- lib/core/services/router.dart
+++ lib/core/services/router.dart
@@ -5,11 +5,15 @@ import 'package:jadwal_solat/core/services/injection_container.dart';
 import 'package:jadwal_solat/features/home/presentation/pages/main_page.dart';
 import 'package:jadwal_solat/features/prayer_time/presentation/bloc/prayer_time_bloc.dart';
 import 'package:jadwal_solat/features/prayer_time/presentation/pages/prayer_time_page.dart';
+import 'package:jadwal_solat/features/quran/presentation/bloc/quran_bloc.dart';
+import 'package:jadwal_solat/features/quran/presentation/pages/quran_detail_page.dart';
+import 'package:jadwal_solat/features/quran/presentation/pages/quran_page.dart';
 
 enum Routes {
   home,
   jadwal,
-  map,
+  quran,
+  quranDetail,
 }
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
@@ -32,6 +36,21 @@ final router = GoRouter(
         create: (context) => sl<PrayerTimeBloc>(),
         child: const PrayerTimePage(),
       ),
+    ),
+     GoRoute(
+      path: '/quran',
+      name: Routes.quran.name,
+      builder: (context, state) => BlocProvider(
+        create: (context) => sl<QuranBloc>(),
+        child: const QuranPage(),
+      ),
+      routes: [
+        GoRoute(
+          path: 'quran-detail',
+          name: Routes.quranDetail.name,
+          builder: (context, state) => const QuranDetailPage(),
+        ),
+      ],
     ),
   ],
 );
---
+++ lib/features/quran/presentation/pages/quran_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:jadwal_solat/core/constants/colours.dart';
+
+class QuranPage extends StatelessWidget {
+  const QuranPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colours.backgroundColor,
+      body: ListView(
+        
+      ),
+    );
+  }
+}
