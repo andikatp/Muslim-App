395698460507f9033b452fdeadcc36d2311f4c2f
andikatp <triprasetya_andika@yahoo.com>
2024-01-30 16:13:29 +0700

    bloc quran

--- lib/features/quran/presentation/bloc/quran_bloc.dart
+++ lib/features/quran/presentation/bloc/quran_bloc.dart
@@ -2,15 +2,48 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jadwal_solat/features/quran/domain/entities/quran_content_entity.dart';
 import 'package:jadwal_solat/features/quran/domain/entities/quran_entity.dart';
+import 'package:jadwal_solat/features/quran/domain/usecases/get_quran_content.dart';
+import 'package:jadwal_solat/features/quran/domain/usecases/get_qurans.dart';
 import 'package:meta/meta.dart';
 
 part 'quran_event.dart';
 part 'quran_state.dart';
 
 class QuranBloc extends Bloc<QuranEvent, QuranState> {
-  QuranBloc() : super(const QuranInitial()) {
+  QuranBloc({
+    required GetQurans getQurans,
+    required GetQuranContent getQuranContent,
+  })  : _getQurans = getQurans,
+        _getQuranContent = getQuranContent,
+        super(const QuranInitial()) {
     on<QuranEvent>((event, emit) {
       emit(const QuranContentLoading());
     });
+    on<GetQuransEvent>(_getQuransHandler);
+    on<GetQuranContentEvent>(_getQuranContentHandler);
+  }
+  final GetQurans _getQurans;
+  final GetQuranContent _getQuranContent;
+
+  Future<void> _getQuransHandler(
+    GetQuransEvent event,
+    Emitter<QuranState> emit,
+  ) async {
+    final result = await _getQurans();
+    result.fold(
+      (failure) => emit(QuranError(message: failure.errorMessage)),
+      (qurans) => emit(QuranLoaded(qurans: qurans)),
+    );
+  }
+
+  Future<void> _getQuranContentHandler(
+    GetQuranContentEvent event,
+    Emitter<QuranState> emit,
+  ) async {
+    final result = await _getQuranContent(event.id);
+    result.fold(
+      (failure) => emit(QuranError(message: failure.errorMessage)),
+      (quran) => emit(QuranContentLoaded(quran: quran)),
+    );
   }
 }
--- lib/features/quran/presentation/bloc/quran_event.dart
+++ lib/features/quran/presentation/bloc/quran_event.dart
@@ -1,4 +1,21 @@
 part of 'quran_bloc.dart';
 
 @immutable
-sealed class QuranEvent {}
+sealed class QuranEvent extends Equatable {
+  const QuranEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class GetQuransEvent extends QuranEvent {
+  const GetQuransEvent();
+}
+
+class GetQuranContentEvent extends QuranEvent {
+  const GetQuranContentEvent({required this.id});
+  final int id;
+
+  @override
+  List<Object> get props => [id];
+}
