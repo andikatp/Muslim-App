97074397a162664f1de7cc0f29741140c04227a6
andikatp <triprasetya_andika@yahoo.com>
2024-01-30 13:02:39 +0700

    calendar widget

--- lib/features/prayer_time/presentation/pages/prayer_time_page.dart
+++ lib/features/prayer_time/presentation/pages/prayer_time_page.dart
@@ -2,15 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jadwal_solat/features/prayer_time/presentation/bloc/prayer_time_bloc.dart';
 import 'package:jadwal_solat/features/prayer_time/presentation/widgets/app_bar_jadwal_widget.dart';
+import 'package:jadwal_solat/features/prayer_time/presentation/widgets/calendar_widget.dart';
 
-class PrayerTimePage extends StatelessWidget {
+class PrayerTimePage extends StatefulWidget {
   const PrayerTimePage({super.key});
 
   @override
+  State<PrayerTimePage> createState() => _PrayerTimePageState();
+}
+
+class _PrayerTimePageState extends State<PrayerTimePage> {
+  DateTime _focusDate = DateTime.now();
+
+  void changeDate(DateTime selectedDate) {
+    setState(() {
+      _focusDate = selectedDate;
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    final now = DateTime.now();
+    context.read<PrayerTimeBloc>().add(
+          GetPrayerTimeEvent(
+            year: now.year,
+            month: now.month,
+            date: now.day,
+          ),
+        );
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    context
-        .read<PrayerTimeBloc>()
-        .add(const GetPrayerTimeEvent(year: 2024, month: 01, date: 30));
     return Scaffold(
       appBar: const AppBarJadwalWidget(),
       body: BlocBuilder<PrayerTimeBloc, PrayerTimeState>(
@@ -20,14 +44,16 @@ class PrayerTimePage extends StatelessWidget {
               child: Text('loading'),
             );
           }
-          if (state is PrayerTimeLoaded) {
-            return Center(
-              child: Text(state.prayerTime.ashar),
-            );
-          }
           if (state is PrayerTimeError) {
             return Center(
               child: Text(state.message),
+            );
+          }
+          if (state is PrayerTimeLoaded) {
+            return Column(
+              children: [
+                CalendarWidget(focusDate: _focusDate, onDateChange: changeDate),
+              ],
             );
           }
           return const SizedBox();
